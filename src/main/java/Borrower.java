3348cc2b078592a13e1ae345bc3458016ddda0c5
lukerhys <hello@lukemelvin.com>
2020-03-24 19:36:49 +0000

    Set up borrower and borrowerTest files let the testing begin

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Borrower {
+
+    private ArrayList<Book> booksOnLoan;
+    private String name;
+
+    public Borrower(String name){
+        this.booksOnLoan = new ArrayList<Book>();
+        this.name = name;
+    }
+
+
 }
--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,2 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class BorrowerTest {
+
+    private Library library;
+    private Book book;
+    private Borrower borrower;
+
+    @Before
+    public void setUp(){
+        library = new Library(10);
+        book = new Book("Absolute Power", "David Baldacci", "Crime Thriller");
+        borrower = new Borrower("Lyndsey");
+    }
+
+    @Test
+    public void cantakeBookFromLibrary(){
+
+    }
 }
